db4a6a121620887eed259eb7472813240a4cc16f
John Carlo De Guzman <johncarlobdeguzman@iskolarngbayan.pup.edu.ph>
2022-07-10 15:31:12 +0800

    combobox/datesss

--- Filtering data.Designer.cs
+++ Filtering data.Designer.cs
@@ -41,21 +41,7 @@
             this.dataaaTableAdapter = new Contact_Tracing_App.Database2DataSet3TableAdapters.dataaaTableAdapter();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.CV20 = new System.Windows.Forms.CheckBox();
-            this.CV21 = new System.Windows.Forms.CheckBox();
-            this.CV23 = new System.Windows.Forms.CheckBox();
-            this.CV22 = new System.Windows.Forms.CheckBox();
-            this.CV24 = new System.Windows.Forms.CheckBox();
-            this.CV25 = new System.Windows.Forms.CheckBox();
-            this.CV26 = new System.Windows.Forms.CheckBox();
-            this.CV27 = new System.Windows.Forms.CheckBox();
-            this.CV28 = new System.Windows.Forms.CheckBox();
-            this.cv4 = new System.Windows.Forms.CheckBox();
-            this.cv3 = new System.Windows.Forms.CheckBox();
-            this.cv2 = new System.Windows.Forms.CheckBox();
-            this.cv1 = new System.Windows.Forms.CheckBox();
-            this.cv30 = new System.Windows.Forms.CheckBox();
-            this.cv29 = new System.Windows.Forms.CheckBox();
+            this.ComboBoxFilter = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataaaBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database2DataSet3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -65,7 +51,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.DarkBlue;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(201, 13);
+            this.button1.Location = new System.Drawing.Point(344, 23);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(76, 28);
@@ -76,10 +62,10 @@
             // 
             // RTBlistofinfo
             // 
-            this.RTBlistofinfo.Location = new System.Drawing.Point(64, 51);
+            this.RTBlistofinfo.Location = new System.Drawing.Point(57, 61);
             this.RTBlistofinfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.RTBlistofinfo.Name = "RTBlistofinfo";
-            this.RTBlistofinfo.Size = new System.Drawing.Size(660, 204);
+            this.RTBlistofinfo.Size = new System.Drawing.Size(648, 204);
             this.RTBlistofinfo.TabIndex = 2;
             this.RTBlistofinfo.Text = "";
             // 
@@ -87,10 +73,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(300, 17);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 21);
+            this.label1.Size = new System.Drawing.Size(252, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "All Gathered Information";
             // 
@@ -122,21 +108,21 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(147, 278);
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(82, 286);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 21);
+            this.label4.Size = new System.Drawing.Size(211, 25);
             this.label4.TabIndex = 97;
-            this.label4.Text = "Filter Information";
+            this.label4.Text = "Filter Information by";
             // 
             // loadbtn
             // 
             this.loadbtn.BackColor = System.Drawing.Color.DarkBlue;
             this.loadbtn.ForeColor = System.Drawing.Color.White;
-            this.loadbtn.Location = new System.Drawing.Point(361, 271);
+            this.loadbtn.Location = new System.Drawing.Point(439, 285);
             this.loadbtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.loadbtn.Name = "loadbtn";
-            this.loadbtn.Size = new System.Drawing.Size(85, 28);
+            this.loadbtn.Size = new System.Drawing.Size(73, 28);
             this.loadbtn.TabIndex = 98;
             this.loadbtn.Text = "Load";
             this.loadbtn.UseVisualStyleBackColor = false;
@@ -149,9 +135,9 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(64, 392);
+            this.dataGridView2.Location = new System.Drawing.Point(57, 333);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(660, 124);
+            this.dataGridView2.Size = new System.Drawing.Size(648, 193);
             this.dataGridView2.TabIndex = 99;
             // 
             // contextMenuStrip1
@@ -159,185 +145,29 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // CV20
+            // ComboBoxFilter
             // 
-            this.CV20.AutoSize = true;
-            this.CV20.BackColor = System.Drawing.Color.Transparent;
-            this.CV20.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV20.Location = new System.Drawing.Point(125, 310);
-            this.CV20.Name = "CV20";
-            this.CV20.Size = new System.Drawing.Size(90, 20);
-            this.CV20.TabIndex = 101;
-            this.CV20.Text = "20/06/2022";
-            this.CV20.UseVisualStyleBackColor = false;
-            // 
-            // CV21
-            // 
-            this.CV21.AutoSize = true;
-            this.CV21.BackColor = System.Drawing.Color.Transparent;
-            this.CV21.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV21.Location = new System.Drawing.Point(125, 336);
-            this.CV21.Name = "CV21";
-            this.CV21.Size = new System.Drawing.Size(90, 20);
-            this.CV21.TabIndex = 105;
-            this.CV21.Text = "21/06/2022";
-            this.CV21.UseVisualStyleBackColor = false;
-            // 
-            // CV23
-            // 
-            this.CV23.AutoSize = true;
-            this.CV23.BackColor = System.Drawing.Color.Transparent;
-            this.CV23.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV23.Location = new System.Drawing.Point(236, 310);
-            this.CV23.Name = "CV23";
-            this.CV23.Size = new System.Drawing.Size(90, 20);
-            this.CV23.TabIndex = 106;
-            this.CV23.Text = "23/06/2022";
-            this.CV23.UseVisualStyleBackColor = false;
-            // 
-            // CV22
-            // 
-            this.CV22.AutoSize = true;
-            this.CV22.BackColor = System.Drawing.Color.Transparent;
-            this.CV22.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV22.Location = new System.Drawing.Point(125, 361);
-            this.CV22.Name = "CV22";
-            this.CV22.Size = new System.Drawing.Size(90, 20);
-            this.CV22.TabIndex = 107;
-            this.CV22.Text = "22/06/2022";
-            this.CV22.UseVisualStyleBackColor = false;
-            // 
-            // CV24
-            // 
-            this.CV24.AutoSize = true;
-            this.CV24.BackColor = System.Drawing.Color.Transparent;
-            this.CV24.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV24.Location = new System.Drawing.Point(236, 335);
-            this.CV24.Name = "CV24";
-            this.CV24.Size = new System.Drawing.Size(90, 20);
-            this.CV24.TabIndex = 108;
-            this.CV24.Text = "24/06/2022";
-            this.CV24.UseVisualStyleBackColor = false;
-            // 
-            // CV25
-            // 
-            this.CV25.AutoSize = true;
-            this.CV25.BackColor = System.Drawing.Color.Transparent;
-            this.CV25.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV25.Location = new System.Drawing.Point(236, 361);
-            this.CV25.Name = "CV25";
-            this.CV25.Size = new System.Drawing.Size(90, 20);
-            this.CV25.TabIndex = 109;
-            this.CV25.Text = "25/06/2022";
-            this.CV25.UseVisualStyleBackColor = false;
-            // 
-            // CV26
-            // 
-            this.CV26.AutoSize = true;
-            this.CV26.BackColor = System.Drawing.Color.Transparent;
-            this.CV26.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV26.Location = new System.Drawing.Point(347, 310);
-            this.CV26.Name = "CV26";
-            this.CV26.Size = new System.Drawing.Size(90, 20);
-            this.CV26.TabIndex = 110;
-            this.CV26.Text = "26/06/2022";
-            this.CV26.UseVisualStyleBackColor = false;
-            // 
-            // CV27
-            // 
-            this.CV27.AutoSize = true;
-            this.CV27.BackColor = System.Drawing.Color.Transparent;
-            this.CV27.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV27.Location = new System.Drawing.Point(347, 335);
-            this.CV27.Name = "CV27";
-            this.CV27.Size = new System.Drawing.Size(90, 20);
-            this.CV27.TabIndex = 111;
-            this.CV27.Text = "27/06/2022";
-            this.CV27.UseVisualStyleBackColor = false;
-            // 
-            // CV28
-            // 
-            this.CV28.AutoSize = true;
-            this.CV28.BackColor = System.Drawing.Color.Transparent;
-            this.CV28.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CV28.Location = new System.Drawing.Point(347, 361);
-            this.CV28.Name = "CV28";
-            this.CV28.Size = new System.Drawing.Size(90, 20);
-            this.CV28.TabIndex = 112;
-            this.CV28.Text = "28/06/2022";
-            this.CV28.UseVisualStyleBackColor = false;
-            // 
-            // cv4
-            // 
-            this.cv4.AutoSize = true;
-            this.cv4.BackColor = System.Drawing.Color.Transparent;
-            this.cv4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cv4.Location = new System.Drawing.Point(576, 361);
-            this.cv4.Name = "cv4";
-            this.cv4.Size = new System.Drawing.Size(90, 20);
-            this.cv4.TabIndex = 118;
-            this.cv4.Text = "04/07/2022";
-            this.cv4.UseVisualStyleBackColor = false;
-            // 
-            // cv3
-            // 
-            this.cv3.AutoSize = true;
-            this.cv3.BackColor = System.Drawing.Color.Transparent;
-            this.cv3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cv3.Location = new System.Drawing.Point(576, 335);
-            this.cv3.Name = "cv3";
-            this.cv3.Size = new System.Drawing.Size(90, 20);
-            this.cv3.TabIndex = 117;
-            this.cv3.Text = "03/07/2022";
-            this.cv3.UseVisualStyleBackColor = false;
-            // 
-            // cv2
-            // 
-            this.cv2.AutoSize = true;
-            this.cv2.BackColor = System.Drawing.Color.Transparent;
-            this.cv2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cv2.Location = new System.Drawing.Point(576, 310);
-            this.cv2.Name = "cv2";
-            this.cv2.Size = new System.Drawing.Size(90, 20);
-            this.cv2.TabIndex = 116;
-            this.cv2.Text = "02/07/2022";
-            this.cv2.UseVisualStyleBackColor = false;
-            // 
-            // cv1
-            // 
-            this.cv1.AutoSize = true;
-            this.cv1.BackColor = System.Drawing.Color.Transparent;
-            this.cv1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cv1.Location = new System.Drawing.Point(465, 361);
-            this.cv1.Name = "cv1";
-            this.cv1.Size = new System.Drawing.Size(90, 20);
-            this.cv1.TabIndex = 115;
-            this.cv1.Text = "01/07/2022";
-            this.cv1.UseVisualStyleBackColor = false;
-            // 
-            // cv30
-            // 
-            this.cv30.AutoSize = true;
-            this.cv30.BackColor = System.Drawing.Color.Transparent;
-            this.cv30.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cv30.Location = new System.Drawing.Point(465, 335);
-            this.cv30.Name = "cv30";
-            this.cv30.Size = new System.Drawing.Size(90, 20);
-            this.cv30.TabIndex = 114;
-            this.cv30.Text = "30/06/2022";
-            this.cv30.UseVisualStyleBackColor = false;
-            // 
-            // cv29
-            // 
-            this.cv29.AutoSize = true;
-            this.cv29.BackColor = System.Drawing.Color.Transparent;
-            this.cv29.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cv29.Location = new System.Drawing.Point(465, 310);
-            this.cv29.Name = "cv29";
-            this.cv29.Size = new System.Drawing.Size(90, 20);
-            this.cv29.TabIndex = 113;
-            this.cv29.Text = "29/06/2022";
-            this.cv29.UseVisualStyleBackColor = false;
+            this.ComboBoxFilter.FormattingEnabled = true;
+            this.ComboBoxFilter.Items.AddRange(new object[] {
+            "20/06/2022",
+            "21/06/2022",
+            "22/06/2022",
+            "23/06/2022",
+            "24/06/2022",
+            "25/06/2022",
+            "26/06/2022",
+            "27/06/2022",
+            "28/06/2022",
+            "29/06/2022",
+            "30/06/2022",
+            "01/07/2022",
+            "02/07/2022",
+            "03/07/2022",
+            "04/07/2022"});
+            this.ComboBoxFilter.Location = new System.Drawing.Point(306, 285);
+            this.ComboBoxFilter.Name = "ComboBoxFilter";
+            this.ComboBoxFilter.Size = new System.Drawing.Size(114, 28);
+            this.ComboBoxFilter.TabIndex = 119;
             // 
             // filterForm
             // 
@@ -346,21 +176,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(842, 572);
-            this.Controls.Add(this.cv4);
-            this.Controls.Add(this.cv3);
-            this.Controls.Add(this.cv2);
-            this.Controls.Add(this.cv1);
-            this.Controls.Add(this.cv30);
-            this.Controls.Add(this.cv29);
-            this.Controls.Add(this.CV28);
-            this.Controls.Add(this.CV27);
-            this.Controls.Add(this.CV26);
-            this.Controls.Add(this.CV25);
-            this.Controls.Add(this.CV24);
-            this.Controls.Add(this.CV22);
-            this.Controls.Add(this.CV23);
-            this.Controls.Add(this.CV21);
-            this.Controls.Add(this.CV20);
+            this.Controls.Add(this.ComboBoxFilter);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.loadbtn);
             this.Controls.Add(this.label4);
@@ -394,20 +210,6 @@
         private Database2DataSet3TableAdapters.dataaaTableAdapter dataaaTableAdapter;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.CheckBox CV20;
-        private System.Windows.Forms.CheckBox CV21;
-        private System.Windows.Forms.CheckBox CV23;
-        private System.Windows.Forms.CheckBox CV22;
-        private System.Windows.Forms.CheckBox CV24;
-        private System.Windows.Forms.CheckBox CV25;
-        private System.Windows.Forms.CheckBox CV26;
-        private System.Windows.Forms.CheckBox CV27;
-        private System.Windows.Forms.CheckBox CV28;
-        private System.Windows.Forms.CheckBox cv4;
-        private System.Windows.Forms.CheckBox cv3;
-        private System.Windows.Forms.CheckBox cv2;
-        private System.Windows.Forms.CheckBox cv1;
-        private System.Windows.Forms.CheckBox cv30;
-        private System.Windows.Forms.CheckBox cv29;
+        private System.Windows.Forms.ComboBox ComboBoxFilter;
     }
 }
--- Filtering data.cs
+++ Filtering data.cs
@@ -59,7 +59,7 @@ namespace Contact_Tracing_App
 
         void filter()
         {
-            if (CV20.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("20/06/2022"))
             {
                 DataTable dt = new DataTable();
                 dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
@@ -74,7 +74,7 @@ namespace Contact_Tracing_App
 
                 dataGridView2.DataSource = dt;
             }
-            if (CV21.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("21/06/2022"))
             {
                 DataTable dt = new DataTable();
                 dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
@@ -88,7 +88,7 @@ namespace Contact_Tracing_App
 
                 dataGridView2.DataSource = dt;
             }
-            if (CV22.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("22/06/2022"))
             {
                 DataTable dt = new DataTable();
                 dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
@@ -102,8 +102,8 @@ namespace Contact_Tracing_App
 
                 dataGridView2.DataSource = dt;
             }
-            if (CV23.Checked)
-            {
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("23/06/2022"))
+                {
                 DataTable dt = new DataTable();
                 dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
                 dt.Columns.Add("Email", Type.GetType("System.String"));
@@ -117,7 +117,7 @@ namespace Contact_Tracing_App
 
                 dataGridView2.DataSource = dt;
             }
-            if (CV24.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("24/06/2022"))
             {
                 DataTable dt = new DataTable();
                 dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
@@ -130,7 +130,11 @@ namespace Contact_Tracing_App
                 dt.Rows.Add("24/06/2022", "johnbarrientes@gmail.com", "John Cruz Barrientes", "19", "Nov. 18, 2002", "San Juan San Ildefonso Bulacan");
                 dataGridView2.DataSource = dt;
             }
-            if (CV26.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("25/06/2022"))
+            {
+                MessageBox.Show("No Information");
+            }
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("26/06/2022"))
             {
                 DataTable dt = new DataTable();
                 dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
@@ -143,71 +147,27 @@ namespace Contact_Tracing_App
                 dt.Rows.Add("26/06/2022", "JCDEGUZMAN@GMAIL.COM", "JOHN CRUZ GUZMAN", "19", "OCT, 1, 2002", "CALASAG SAN ILDEFONSO BULCAN");
                 dataGridView2.DataSource = dt;
             }
-            if (CV20.Checked && CV21.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("27/06/2022"))
             {
-                DataTable dt = new DataTable();
-                dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
-                dt.Columns.Add("Email", Type.GetType("System.String"));
-                dt.Columns.Add("Name", Type.GetType("System.String"));
-                dt.Columns.Add("Age", Type.GetType("System.Int32"));
-                dt.Columns.Add("Birthday", Type.GetType("System.String"));
-                dt.Columns.Add("Address", Type.GetType("System.String"));
-
-                dt.Rows.Add("20/06/2022", "jcdeguzman1218@gmail.com", "John Carlo Barrientes De Guzman", "19", "Dec. 18, 2002", "Lapnit San Ildefonso Bulacan");
-                dt.Rows.Add("20/06/2022", "jpubaldo19@gmail.com", "John Paulo Ubaldo De Guzman", "19", "Aug. 04, 2002", "Lapnit San Ildefonso Bulacan");
-                dt.Rows.Add("21/06/2022", "jkbarrientes@gmail.com", "John Kurt Cruz Barrientes", "19", "Oct. 12, 2002", "Lapnit San Ildefonso Bulacan");
-                dataGridView2.DataSource = dt;
+                MessageBox.Show("No Information");
             }
-            if (CV20.Checked && CV21.Checked && CV22.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("28/06/2022"))
             {
-                DataTable dt = new DataTable();
-                dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
-                dt.Columns.Add("Email", Type.GetType("System.String"));
-                dt.Columns.Add("Name", Type.GetType("System.String"));
-                dt.Columns.Add("Age", Type.GetType("System.Int32"));
-                dt.Columns.Add("Birthday", Type.GetType("System.String"));
-                dt.Columns.Add("Address", Type.GetType("System.String"));
-
-                dt.Rows.Add("20/06/2022", "jcdeguzman1218@gmail.com", "John Carlo Barrientes De Guzman", "19", "Dec. 18, 2002", "Lapnit San Ildefonso Bulacan");
-                dt.Rows.Add("20/06/2022", "jpubaldo19@gmail.com", "John Paulo Ubaldo De Guzman", "19", "Aug. 04, 2002", "Lapnit San Ildefonso Bulacan");
-                dt.Rows.Add("21/06/2022", "jkbarrientes@gmail.com", "John Kurt Cruz Barrientes", "19", "Oct. 12, 2002", "Lapnit San Ildefonso Bulacan");
-                dt.Rows.Add("22/06/2022", "joeydg@gmail.com", "Joey Cruz De Guzman", "20", "Jan. 24, 2002", "San Juan San Ildefonso Bulacan");
-                dataGridView2.DataSource = dt;
+                MessageBox.Show("No Information");
             }
-            if (CV23.Checked && CV24.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("29/06/2022"))
             {
-                DataTable dt = new DataTable();
-                dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
-                dt.Columns.Add("Email", Type.GetType("System.String"));
-                dt.Columns.Add("Name", Type.GetType("System.String"));
-                dt.Columns.Add("Age", Type.GetType("System.Int32"));
-                dt.Columns.Add("Birthday", Type.GetType("System.String"));
-                dt.Columns.Add("Address", Type.GetType("System.String"));
-
-                dt.Rows.Add("23/06/2022", "jamesbarrientes@gmail.com", "James Liwag Barrientes", "20", "March 24, 2002", "Calawitan San Ildefonso Bulacan");
-                dt.Rows.Add("23/06/2022", "pauldelacruz@gmail.com", "Paul Liwag Dela Cruz", "19", "Dec. 08, 2002", "Malipampang San Ildefonso Bulacan");
-                dt.Rows.Add("24/06/2022", "johnbarrientes@gmail.com", "John Cruz Barrientes", "19", "Nov. 18, 2002", "San Juan San Ildefonso Bulacan");
-
-                dataGridView2.DataSource = dt;
+                MessageBox.Show("No Information");
             }
-            if (CV23.Checked && CV24.Checked && CV26.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("30/06/2022"))
             {
-                DataTable dt = new DataTable();
-                dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
-                dt.Columns.Add("Email", Type.GetType("System.String"));
-                dt.Columns.Add("Name", Type.GetType("System.String"));
-                dt.Columns.Add("Age", Type.GetType("System.Int32"));
-                dt.Columns.Add("Birthday", Type.GetType("System.String"));
-                dt.Columns.Add("Address", Type.GetType("System.String"));
-
-                dt.Rows.Add("23/06/2022", "jamesbarrientes@gmail.com", "James Liwag Barrientes", "20", "March 24, 2002", "Calawitan San Ildefonso Bulacan");
-                dt.Rows.Add("23/06/2022", "pauldelacruz@gmail.com", "Paul Liwag Dela Cruz", "19", "Dec. 08, 2002", "Malipampang San Ildefonso Bulacan");
-                dt.Rows.Add("24/06/2022", "johnbarrientes@gmail.com", "John Cruz Barrientes", "19", "Nov. 18, 2002", "San Juan San Ildefonso Bulacan");
-                dt.Rows.Add("26/06/2022", "JCDEGUZMAN@GMAIL.COM", "JOHN CRUZ GUZMAN", "19", "OCT, 1, 2002", "CALASAG SAN ILDEFONSO BULCAN");
-                dataGridView2.DataSource = dt;
-
+                MessageBox.Show("No Information");
             }
-            if (cv2.Checked)
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("01/07/2022"))
+            {
+                MessageBox.Show("No Information");
+            }
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("02/07/2022"))
             {
                 DataTable dt = new DataTable();
                 dt.Columns.Add("DateLogin", Type.GetType("System.DateTime"));
@@ -218,6 +178,14 @@ namespace Contact_Tracing_App
                 dt.Columns.Add("Address", Type.GetType("System.String"));
                 dt.Rows.Add("02/07/2022", "jcdeguzman1218@gmail.com", "John Carlo Barrientes De Guzman", "19", "Dec. 18, 2002", "Lapnit San Ildefonso Bulacan");
                 dataGridView2.DataSource = dt;
+            }
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("03/07/2022"))
+            {
+                MessageBox.Show("No Information");
+            }
+            if (ComboBoxFilter.SelectedItem.ToString().Contains("04/07/2022"))
+            {
+                MessageBox.Show("No Information");
             }
         }
     }
